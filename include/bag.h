e7eaa24e96ddc2cf6390769d6e9bf8d4a4f8376a
anastasia-tuttle <111314106+anastasia-tuttle@users.noreply.github.com>
2022-08-28 18:10:16 -0500

    class

---
+++ include/bag.h
@@ -0,0 +1,44 @@
+//
+// File:   bag.h
+// Author: Your Glorious Instructor
+// Purpose:
+// Implement the Bag ADT.
+//
+#pragma once
+#include <iostream>
+#include "vector.h"
+
+template <typename Thing>
+class Bag {
+public:
+  void insert(Thing aThing) {
+    bagContents.push_back(aThing);
+    bagSize++;
+  }
+  Thing &pop() {
+    Thing aThing;
+    if (bagContents.size() > 0) {
+      aThing = bagContents[bagSize];
+      bagSize--;
+    }
+    else {
+      std::cerr << "Can't pop out of an empty bag" << std::endl;
+    }
+    return aThing;
+  }
+  int size() {
+    return bagSize;
+  }
+  int count(Thing aThing) {
+    int bagCount = 0;
+    for (int i = 0 ; i < bagSize; i++) {
+      if (bagContents[i] == aThing) {
+        bagCount++;
+      }
+    }
+    return bagCount;
+  }
+private:
+  vector<Thing> bagContents;
+  int bagSize = 0;
+};
