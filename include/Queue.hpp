abc12e02cd37ffced66c142aa21900c3b723abac
anastasia-tuttle <111314106+anastasia-tuttle@users.noreply.github.com>
2022-09-19 18:31:30 -0500

    Create Queue.hpp

---
+++ include/Queue.hpp
@@ -0,0 +1,22 @@
+
+
+#pragma once
+#include "List.hpp"
+template <typename T>
+class Queue {
+private:
+   List<T> queueList;
+public:
+   Queue() {}
+   Queue(Queue &rhs) {}
+   ~Queue() { }
+   bool  empty() {return queueList.empty();}
+   void push(T data) {queueList.push_front(data);}
+   T front() { return queueList.front(); }
+   T back() { return queueList.back(); }
+   void pop() { queueList.pop_back();}
+   void traverse(void (*doIt)(T &data)){
+      queueList.traverse(doIt);
+   };
+
+};
